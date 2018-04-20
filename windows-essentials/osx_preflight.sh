08fc8289340b56413ae1fa688290c1e5bf82684e
stellarsquall <stellarsquall@protonmail.ch>
2018-04-20 15:55:08 -0600

    removed check for X-Code Dev tools

--- windows-essentials/osx_preflight.sh
+++ windows-essentials/osx_preflight.sh
@@ -7,23 +7,6 @@ green=$(tput setaf 2)
 normal=$(tput sgr0)
 col=40
 
-# Are the XCode Developer Tools installed?
-echo
-echo "###############################################################################"
-echo "Verifying XCode Developer Tools are Installed..."
-echo 
-
-# Think this is Yosemite only. Changing to a more inclusive command, and may just add some conditional logic in the future.
-# xcode-select -p 2>&1 >/dev/null
-pkgutil --pkg-info=com.apple.pkg.CLTools_Executables 2>&1 >/dev/null
-
-
-if [ $? -eq 0 ]; then
-  printf '%-50s%*s%s\n' "Checking for XCode Developer Tools" $col "${green}[OK]${normal}"
-else
-  printf '%-50s%*s%s\n' "Checking for XCode Developer Tools" $col "${red}[FAIL]${normal}"
-fi
-
 # Sanity Check: Do I have gcc and make?
 
 # Define tools here to keep things DRY
